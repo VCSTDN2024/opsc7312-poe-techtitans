689dad512b1c51b45f9f8ad966b8bee37fbac10b
Matteo Pita <ST10024454@vcconnect.edu.za>
2024-09-12 09:48:02 +0200

    Conversions frontend page done

---
+++ app/src/main/java/com/example/fusion/ConversionsPage.kt
@@ -0,0 +1,16 @@
+package com.example.fusion
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ConversionsPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_conversions_page)
+
+    }
+}
